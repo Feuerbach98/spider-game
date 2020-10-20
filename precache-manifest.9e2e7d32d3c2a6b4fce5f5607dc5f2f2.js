40b19a40dbbb31b307cec01854eb1b80137a2f7c
Nikolay Filiuk <Filiuk98@gmail.com>
2020-10-20 14:41:05 +0300

    Updates

---
+++ precache-manifest.9e2e7d32d3c2a6b4fce5f5607dc5f2f2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9682f10c4f6a52d4042da9a02a5956",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "f543da5f4e5be5c1a4d2",
+    "url": "/react-gh-pages/static/css/main.ac8ff84f.chunk.css"
+  },
+  {
+    "revision": "e034d9fda46d3498b184",
+    "url": "/react-gh-pages/static/js/2.384e9600.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-gh-pages/static/js/2.384e9600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f543da5f4e5be5c1a4d2",
+    "url": "/react-gh-pages/static/js/main.e7984fc4.chunk.js"
+  },
+  {
+    "revision": "cac4a34002b4e0a499ea",
+    "url": "/react-gh-pages/static/js/runtime-main.1c1602b7.js"
+  },
+  {
+    "revision": "35cd5e2f19a03add1bdb7352c4ebf889",
+    "url": "/react-gh-pages/static/media/brick-wall.35cd5e2f.svg"
+  },
+  {
+    "revision": "8fcaccea3b63f4c5a6dfbb6668a69fe8",
+    "url": "/react-gh-pages/static/media/fly.8fcaccea.png"
+  },
+  {
+    "revision": "b9d55c58755bb2eecc96e49ad154987f",
+    "url": "/react-gh-pages/static/media/spider.b9d55c58.svg"
+  }
+]);
